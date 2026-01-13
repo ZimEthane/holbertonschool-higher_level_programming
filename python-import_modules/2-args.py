4fb1787abb4232ba756f366588880840cc360a93
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-13 14:21:25 +0000

    feat : create a program that prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arg_count = len(sys.argv) - 1
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count != 0:
+        print("{} argument{}:".format(
+            arg_count,
+            "" if arg_count == 1 else "s"
+        ))
+    for i in range(1, arg_count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+        
