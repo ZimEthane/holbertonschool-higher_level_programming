f983c75efa9c3e2a8d3080df9f10724663674e29
ZimEthane <ethane.zim@hotmail.com>
2026-01-16 08:50:55 +0000

    feat : create a program that prints all the names defined by the compiled module hidden_4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-import hiden_4.pyc
+import hidden_4
+
+if __name__ == "__main__":
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
