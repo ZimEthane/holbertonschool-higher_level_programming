4d388709268e0da82bdcc58e5c67965578d8e335
ZimEthane <ethane.zim@hotmail.com>
2026-01-16 09:34:01 +0000

    fix : update code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-
 if __name__ == "__main__":
-    for name in sorted(dir(hidden_4)):
+    names = dir(hidden_4)
+    for name in names:
         if not name.startswith("__"):
             print(name)
