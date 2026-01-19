bb203be1fef10fd426cdfb81ff905d2c3541546c
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-19 10:49:41 +0000

    feat : create a function that prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for first_idx in matrix:
+        for second_idx in matrix[first_idx]:
+            if second_idx == matrix[i][-1]:
+                print("{:d}".format(second_idx), end="")
+            else:
+                print("{:d}".format(second_idx), end=" ")
+        print("")
