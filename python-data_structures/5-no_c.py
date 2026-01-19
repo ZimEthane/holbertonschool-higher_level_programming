ab50c4fbadae2ef8e4389dbb98374757d406c87d
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-19 10:36:31 +0000

    fix : change condition

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for char in (my_string):
-        if char == 'c' or char == 'C':
-            my_string.remove(char)
+        if char != 'c' and char != 'C':
+            new_string += char
     return my_string
