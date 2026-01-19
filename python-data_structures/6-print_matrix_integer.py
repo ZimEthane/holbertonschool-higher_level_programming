3446186720110f2863f37ac79c912fab3384e846
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-19 10:51:32 +0000

    fix : update code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for first_idx in range(0, len(matrix)):
+    for first_idx in matrix:
         for second_idx in matrix[first_idx]:
-            if second_idx == matrix[i][-1]:
+            if second_idx == matrix[first_idx][-1]:
                 print("{:d}".format(second_idx), end="")
             else:
                 print("{:d}".format(second_idx), end=" ")
