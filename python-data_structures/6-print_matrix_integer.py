513c16dc4656747e56186414fdd09dc63f45292d
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-19 10:50:47 +0000

    fixs : change condition in first loops

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for first_idx in matrix:
+    for first_idx in range(0, len(matrix)):
         for second_idx in matrix[first_idx]:
             if second_idx == matrix[i][-1]:
                 print("{:d}".format(second_idx), end="")
