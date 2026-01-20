61be906fd63f48b1f768d6e47788133ac16cdb07
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-20 10:24:06 +0000

    fix : update condition

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,8 @@ def only_diff_elements(set_1, set_2):
     new_list = []
     for i in set_1:
         for j in set_2:
-            if i != j:
+            if i == j:
+                continue
+            else:
                 new_list.append(i)
     return new_list
