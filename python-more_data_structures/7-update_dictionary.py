9ebf7d83d6167c2023015051155a5a20608aede8
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-20 13:41:23 +0000

    fix: update code

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,5 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     if a_dictionary[key]:
         a_dictionary[key] = value
     else:
-        a_dictionary.setdefault(key: value)
+        a_dictionary.setdefault(key, value)
     return a_dictionary
