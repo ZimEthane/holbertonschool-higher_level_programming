4f59f5ee04b7ebe2192200090d1b2f9209bb824a
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-20 09:34:13 +0000

    fix : remanind the code

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list.replace(search, replace)
+    for i in range(0, len(my_list)):
+        if i == search:
+            my_list[i] = replace
+    return my_list
