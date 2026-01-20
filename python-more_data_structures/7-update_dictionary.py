96224f1a80f14e9f32e61e2d7e31fa721a0014cd
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-20 13:44:29 +0000

    fixn+ : delete condition

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary[key]:
-        a_dictionary[key] = value
-    else:
-        a_dictionary.setdefault(key, value)
+    a_dictionary[key] = value
     return a_dictionary
