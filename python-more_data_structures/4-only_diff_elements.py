bd1d8562db6912bfc293af5f3f4d38be0edd9ee6
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-20 13:06:38 +0000

    fix : update to other methode

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = []
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                continue
-            else:
-                new_list.append(i)
-    return new_list
+    # new_set = set()
+
+    # for i in set_1:
+    #     if i not in set_2:
+    #         new_set.add(i)
+    # for j in set_2:
+    #     if j not in set_1:
+    #         new_set.add(j)
+
+    return set_1 - set_2
+
+
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
