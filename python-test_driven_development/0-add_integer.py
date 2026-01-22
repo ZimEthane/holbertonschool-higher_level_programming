ea7bcf5aeeea4ad8f1d72411ea6b39d92886ad7c
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-22 14:14:51 +0000

    feat : create a function that adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Add two integers.
+
+    Args:
+        a (int/float): The first integer.
+        b (int/float): The second integer (default is 98).
+
+    Returns:
+        int: The sum of the two integers.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
