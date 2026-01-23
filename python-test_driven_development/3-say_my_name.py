5a7219a03e59b24bf875cc25aefd3cdb5ff4e303
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-01-23 10:07:55 +0000

    feat : create a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""3-say_my_name module.
-
-This module provides say_my_name(first_name, last_name="").
-It prints: My name is <first_name> <last_name>
-No modules are imported.
+"""
+That a module that contains say_my_name is a function that print
+"My name is" with the first and last name
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Print 'My name is <first_name> <last_name>'."""
-    if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
-        raise TypeError("last_name must be a string")
+    """
+    Function that print My name is <first name> <last name>
 
+    Args:
+        first_name (str): The first name
+        last_name (str): The last name
+
+    Raises:
+        TypeError: If first_name is not a string
+        TypeError: If last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
