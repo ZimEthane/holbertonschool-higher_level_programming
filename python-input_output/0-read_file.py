07e197937c4df12a59d2bc92f226fa984a057213
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-02-11 08:26:04 +0000

    fix : update code, add documentation code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+ module that contains a function that reads a 
+ text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
-    with open(filename, "r") as file:
-        print(file.read())
+    """function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
