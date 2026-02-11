1bb8725f12c3fa246dd484a67162f313512ea40a
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-02-11 09:30:35 +0000

    feat : create a script that adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
+
 """
-This module contains a function that adds all arguments to a Python list,
-and then save them to a file.
+This module contains a function that adds all arguments
+to a Python list, and then save them to a file.
 """
 
+import os
+import sys
 
-import json
-from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file_name = "add_item.json"
 
-def add_item(filename, *args):
-    """
-    Adds all arguments to a Python list, and then save them to a file.
+if os.path.exists(file_name):
+    my_list = load_from_json_file(file_name)
+else:
+    my_list = []
 
-    Args:
-        filename (str): the name of the file where the list will be saved.
-        *args: variable length argument list to be added to the list.
+my_list.extend(sys.argv[1:])
 
-    """
-
-    try:
-        with open(filename, 'r', encoding="utf-8") as file:
-            my_list = json.load(file)
-    except FileNotFoundError:
-        my_list = []
-
-    my_list.extend(args)
-
-    with open(filename, 'w', encoding="utf-8") as file:
-        json.dump(my_list, file)
+save_to_json_file(my_list, file_name)
