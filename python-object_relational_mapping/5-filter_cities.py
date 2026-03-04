d195bfac0703491e114a360153151ff144748f24
ZimEthane <ethane.zim@hotmail.com>
2026-03-04 10:52:05 +0000

    feat : create script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities of a given state from the database hbtn_0e_4_usa
+Arguments: mysql username, mysql password, database name, state name
+SQL injection safe
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+
+    cursor = db.cursor()
+
+    query = """SELECT cities.name
+               FROM cities
+               JOIN states ON cities.state_id = states.id
+               WHERE states.name = %s
+               ORDER BY cities.id ASC"""
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
+
+    cursor.close()
+    db.close()
