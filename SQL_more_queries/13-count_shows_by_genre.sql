14cce76e2605fecb06c871d137d3b3cf604a9d4d
Ethane Zimmermann <ethane.zim@hotmail.com>
2026-03-06 09:50:14 +0100

    fix : Modify SQL script to filter genres by show 'Dexter'
    Updated the SQL script to list genres specifically for the show 'Dexter'.

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
--- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+-- Script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
