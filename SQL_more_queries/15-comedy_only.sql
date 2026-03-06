19f2d08e36f7dcb4f6ada4b89b98155132aa592c
Ethane Zimmermann <ethane.zim@hotmail.com>
2026-03-06 09:52:03 +0100

    Fix : joins and table names in comedy shows query

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- script that lists all Comedy shows in the database hbtn_0d_tvshows
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON genres.id = tv_show_genres.genre_id
-WHERE genres.name = 'Comedy'
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
